8669376437b6a4c16060524aea51534bf66d6c30
Solacereb <reb.mutanda@hotmail.com>
2025-09-21 22:51:08 +0930

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='solace-firstlight-v1';
-const ASSETS=['/','/index.html','/styles.css','/manifest.json','/assets/solace-logo.png','/assets/icon-192.png','/assets/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+
